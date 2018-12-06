9f76b21dce4edcc3a4efb005ebfeacf850c532c5
Chase <943616235@qq.com>
2018-12-06 16:32:18 +0800

    用户列表开发

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Layout from 'component/layout/index.jsx'
 // 页面
 import Home from 'page/home/index.jsx'
 import Login from 'page/login/index.jsx';
+import UserList from 'page/user/index.jsx';
 import ErrorPage from 'page/error/index.jsx';
 
 class App extends React.Component{
@@ -18,6 +19,8 @@ class App extends React.Component{
             <Layout> 
                 <Switch>
                     <Route exact path="/" component={Home}/>
+                    <Route path="/user/index" component={UserList}/>
+                    <Redirect exact from="/user" to="/user/index"/>
                     <Route component={ErrorPage}/>
                 </Switch>
             </Layout>
